d3e40f6f2633a447f317463aaeefec74b23eb880
Jacek Dogiel <jacek.dogiel@gmail.com>
2022-05-29 12:25:47 +0200

    add styles and fonts

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,16 +3,18 @@ import styled from "styled-components";
 import { Card } from "react-native-paper";
 
 const RestaurantCard = styled(Card)`
-  backgroundcolor: "white";
+  color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  backgroundcolor: "white";
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled.Text`
-  padding: 16px;
+  font-family: ${(props) => props.theme.fonts.heading};
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
