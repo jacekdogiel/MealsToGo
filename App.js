37285cade4236681ed184a54acb5a79dc3aa19a4
Jacek Dogiel <jacek.dogiel@gmail.com>
2022-06-07 10:36:00 +0200

    add account navigator

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
@@ -10,6 +11,21 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_E5_jh2ho4CYfsKXmfdAX3vDXRjn0-8E",
+  authDomain: "mealstogo-51c7d.firebaseapp.com",
+  projectId: "mealstogo-51c7d",
+  storageBucket: "mealstogo-51c7d.appspot.com",
+  messagingSenderId: "174411787590",
+  appId: "1:174411787590:web:ac2dfbd7cc038b8aa64947",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -26,13 +42,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
